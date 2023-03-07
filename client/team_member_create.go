7f233c7476ee52884b10913b1835882ada868c7d
memdealer <wizardgusto@gmail.com>
2023-03-07 21:51:26 +0200

    chores: Implement raise error in case vercel user does not exist

--- client/team_member_create.go
+++ client/team_member_create.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -22,7 +23,6 @@ type createTeamMemberResponse struct {
 
 func (c *Client) CreateTeamMember(ctx context.Context, request CreateTeamMemberRequest) (r TeamMemberResponse, err error) {
 	url := fmt.Sprintf("%s/v1/teams/%s/members", c.baseURL, c._teamID)
-
 	payload := string(mustMarshal(request))
 	req, err := http.NewRequestWithContext(
 		ctx,
@@ -42,6 +42,38 @@ func (c *Client) CreateTeamMember(ctx context.Context, request CreateTeamMemberR
 	err = c.doRequest(req, &teamMemberResponse)
 	if err != nil {
 		return r, err
+	}
+	// Sometimes, the invited user has no account and vercel logic is plain simple is to not return UUID if user has not been created.
+	// I must not argue that such a logic is faulty, as then you should provide either a method to see if user exists on vercel, or at least do not add it to /getTeamMembers call.
+	// Anyhow, I got to invent this bicycle, just to cover cases where UUID is not returned, meaning that the user is not just there, therefore invite is discarded the error issued and all burns.
+	// The logic is: Invite user, asses if UUID is empty, if so -> find UUID of the invite, delete invite, issue an error.
+	// PS: if you know better way, plz issue a PR, otherwise feel free to laugh.
+	//var invitationCode EmailInvitationCodesResponse
+	// I guess this is the correct way to see if the field in struct is empty.
+	if teamMemberResponse.UID == "" {
+		tflog.Info(ctx, "The user HAS NO vercel account, dropping invitation:", map[string]interface{}{})
+		// then we gotta find the UUID of the request we just send
+		invitationCode, err := c.GetTeamMemberInviteCode(ctx, request.Email)
+		// Check again if it is empty, just in case of sudden #YOLO
+		if invitationCode.ID == "" {
+			err = errors.New("failed to get invite code")
+			return TeamMemberResponse{}, err
+		}
+
+		tflog.Info(ctx, "[INVITE CODE] Got this one:", map[string]interface{}{
+			"email": invitationCode.Email,
+			"id":    invitationCode.ID,
+		})
+		err = c.DeleteTeamMemberInviteCode(ctx, invitationCode.ID)
+
+		if err != nil {
+			return TeamMemberResponse{}, err
+		}
+
+		// drop the mic and cover.
+		return TeamMemberResponse{},
+			errors.New(fmt.Sprintf("the vercel user must exist before adding it to the team. \n"+
+				"user in question is: %s", request.Email))
 	}
 
 	return TeamMemberResponse{
--- client/team_member_get.go
+++ client/team_member_get.go
@@ -14,8 +14,68 @@ type TeamMemberResponse struct {
 	Role  string `json:"role"`
 }
 
+type EmailInvitationCodesResponse struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+}
+
 type TeamMemberResponseFlattened struct {
 	Members []TeamMemberResponse `json:"members"`
+}
+
+type InvitationCodesFlattened struct {
+	InvitationCodes []EmailInvitationCodesResponse `json:"emailInviteCodes"`
+}
+
+func (c *Client) GetTeamMemberInviteCode(ctx context.Context, memberEmail string) (r EmailInvitationCodesResponse, err error) {
+	url := fmt.Sprintf("%s/v2/teams/%s/members?search=%s&limit=1", c.baseURL, c._teamID, memberEmail)
+	var flatResp InvitationCodesFlattened
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		url,
+		nil,
+	)
+
+	if err != nil {
+		return r, fmt.Errorf("creating request: %s", err)
+	}
+	tflog.Trace(ctx, "getting team member email codes", map[string]interface{}{
+		"url":   url,
+		"email": memberEmail,
+	})
+
+	err = c.doRequest(req, &flatResp)
+
+	tflog.Trace(ctx, "Get codes response", map[string]interface{}{
+		"InviteCodes:": flatResp.InvitationCodes,
+	})
+
+	if err != nil {
+		tflog.Error(ctx, "Could not retrieve email codes invitation.", map[string]interface{}{
+			"url":   url,
+			"email": memberEmail,
+		})
+		return EmailInvitationCodesResponse{}, err
+	}
+	// iterate over email codes, find the needed one where email == matches with what we are finding.
+	for _, v := range flatResp.InvitationCodes {
+		tflog.Info(ctx, "TeamMember response", map[string]interface{}{
+			"code":  v.ID,
+			"email": v.Email,
+		})
+		if v.Email == memberEmail {
+			return EmailInvitationCodesResponse{
+				ID:    v.ID,
+				Email: v.Email,
+				Role:  v.Role,
+			}, nil
+		}
+	}
+	// if nothing is found, then something wrong and gotta shut down
+	return EmailInvitationCodesResponse{}, nil
 }
 
 func (c *Client) GetTeamMember(ctx context.Context, memberEmail string) (r TeamMemberResponse, err error) {
